2784cfa1971d63b180ff0c6804f4953cd46a492a
Daniel James <daniel@mailbox.org>
2016-09-13 20:25:14 +0100

    Fixed bug with style loading

--- includes/class-commodities-map.php
+++ includes/class-commodities-map.php
@@ -167,7 +167,7 @@ class Commodities_Map {
 
 		$plugin_public = new Commodities_Map_Public( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		$this->loader->add_action( 'wp_enqueue_styles', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 		add_shortcode( 'commodities-map', array( 'Commodities_Map_Public',  'commodities_add_shortcode' ));
